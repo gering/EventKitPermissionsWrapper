e4e99d7b1a4d186ada3655452bf002bf9eeac210
Robert Gering <mail@robertgering.de>
2023-12-07 10:44:57 +0100

    Add C# binding

---
+++ ApiDefinition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Foundation;
+using ObjCRuntime;
+
+namespace EventKitPermissions {
+    [BaseType(typeof(NSObject))]
+    interface EventKitPermissionsWrapper {
+        
+        [Export("authorizationStatusForEvents")]
+        long AuthorizationStatusForEvents();
+
+        [Export("authorizationStatusForReminders")]
+        long AuthorizationStatusForReminders();
+        
+        [Export("requestFullAccessToEventsWithCompletion:")]
+        void RequestFullAccessToEvents(Action<bool, NSError> completion);
+        
+        [Export("requestWriteOnlyAccessToEventsWithCompletion:")]
+        void RequestWriteOnlyAccessToEvents(Action<bool, NSError> completion);
+        
+        [Export("requestFullAccessToRemindersWithCompletion:")]
+        void RequestFullAccessToReminders(Action<bool, NSError> completion);
+    }
+}
